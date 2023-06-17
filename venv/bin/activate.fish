c2aa0bb1c920323034db98bc06ecc08d148877e0
Nigel Phillips <nigelphillips@pursuit.org>
2023-06-17 00:04:43 -0400

    new readme

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nigelphillipspursuit.org/Desktop/myproject/venv"
+set -gx VIRTUAL_ENV "/Users/nigelphillipspursuit.org/Desktop/Database-App/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
